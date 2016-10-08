b1b02a47071be260ff97643fbf94e5c2a81f7312
Wayne Yeo <fishnsotong@gmail.com>
2016-10-09 01:06:53 +0800

    some changes

---
+++ controls.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Raspberry Pi Car Controller></title>
+  </head>
+
+  <body>
+    <button type="button" id="moveForward">Forward</button><br>
+    <button type="button" id="moveReverse">Reverse</button><br>
+  </body>
+</html>
+    
