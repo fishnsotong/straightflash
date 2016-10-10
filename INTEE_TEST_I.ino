7d6c898d789701db3f569bfff0a293c22cd5b706
Wayne Yeo <fishnsotong@gmail.com>
2016-10-10 13:15:04 +0800

    cleaned up vending machine code

--- INTEE_TEST_I.ino
+++ INTEE_TEST_I.ino
@@ -1,17 +1,19 @@
 #include <Servo.h>
-
+const int buttonSwitch = 5;
+const int servoPin = 9;
 Servo servo1;
 
 void setup() 
 {
-  servo1.attach(9);
+  servo1.attach(servoPin);
   pinMode(5, INPUT);
 }
 
 void loop() 
 {
-  const int position1 = 0 ;
-  int switchValue = digitalRead(5);
+  const int position1 = 0;
+  const int position2 = 180;
+  int switchValue = digitalRead(buttonSwitch);
   if (switchValue == LOW)
   {
     servo1.write(position1);
@@ -19,7 +21,7 @@ void loop()
   else 
   {
   servo1.attach(9);
-  servo1.write (180);
+  servo1.write(position2);
   delay(1000);
   }
  
